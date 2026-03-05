f20899a64ac0a8b6349b1203de15f1c31de5a8f0
cfsantdev <cfsant4gamers@gmail.com>
2026-03-05 07:28:19 -0300

    Create monsters.js

---
+++ JS/monsters.js
@@ -0,0 +1,36 @@
+export const monsters = () => {
+    let values = [
+        {
+            id: 1,
+            resourceName: 'Ghost.fbx',
+            resourceTexture: 'Ghost_Texture.png',
+        },
+        {
+            id: 2,
+            resourceName: 'Alien.fbx',
+            resourceTexture: 'Alien_Texture.png',
+        },
+        {
+            id: 3,
+            resourceName: 'Skull.fbx',
+            resourceTexture: 'Skull_Texture.png',
+        },
+        {
+            id: 4,
+            resourceName: 'GreenDemon.fbx',
+            resourceTexture: 'GreenDemon_Texture.png',
+        },
+        {
+            id: 5,
+            resourceName: 'Cyclops.fbx',
+            resourceTexture: 'Cyclops_Texture.png',
+        },
+        {
+            id: 6,
+            resourceName: 'Cactus.fbx',
+            resourceTexture: 'Cactus_Texture.png',
+        }
+    ];
+
+    return values;
+};
